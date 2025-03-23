5d6e22cbc13dbba6f433adb1a2484f8c186ca1dd
qminh3 <165232129+qminh3@users.noreply.github.com>
2025-03-23 14:57:47 +0700

    prepare

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,28 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Doctor from "./pages/Doctor";
+import Login from "./pages/Login";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import MyProfile from "./pages/MyProfile";
+import MyAppointment from "./pages/MyAppointment";
+import Appointment from "./pages/Appointment";
 
 const App = () => {
   return (
-    <div>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    <div className=" mx-4 sm:mx-[10%] ">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/doctors" element={<Doctor />} />
+        <Route path="/doctors/:speciality" element={<Doctor />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/my-profile" element={<MyProfile />} />
+        <Route path="/my-appointments" element={<MyAppointment />} />
+        <Route path="/appointment/:docId" element={<Appointment />} />
+      </Routes>
     </div>
   );
 };
---
+++ frontend/src/pages/Doctor.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Doctor = () => {
+  return (
+    <div>
+      <h1>Doctor Page</h1>
+      <p>Welcome to the Doctor Page</p>
+    </div>
+  );
+};
+
+export default Doctor;
