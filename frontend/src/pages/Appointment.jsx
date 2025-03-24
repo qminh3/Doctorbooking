f1fdf1a8cc4322deeeca0228814aef52d0c70400
qminh3 <165232129+qminh3@users.noreply.github.com>
2025-03-24 13:40:45 +0700

    change

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -1,7 +1,71 @@
-import React from "react";
+import React, { useContext, useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { AppContext } from "../context/AppContext";
+import { assets } from "../assets/assets_frontend/assets";
 
 const Appointment = () => {
-  return <div></div>;
+  // TODO: Implement appointment component here
+  const { docId } = useParams();
+  const { doctors, currencySymbol } = useContext(AppContext);
+  const [docInfo, setDocInfo] = useState(null);
+  const [docSlots, setDocSlots] = useState([]);
+  const [slotIndex, setSlotIndex] = useState(0);
+  const [slotTime, setSlotTime] = useState("");
+
+  const fetchDocInfo = async () => {
+    const docInfo = doctors.find((doc) => doc._id === docId);
+    setDocInfo(docInfo);
+    console.log(docInfo);
+  };
+  useEffect(() => {
+    fetchDocInfo();
+  }, [docId, doctors]);
+
+  return (
+    docInfo && (
+      <div>
+        <div className=" flex flex-col sm:flex-row gap-4">
+          <div>
+            <img
+              className=" bg-[#5f6fff]  w-full sm:max-w-72 rounded-lg "
+              src={docInfo.image}
+              alt=""
+            />
+          </div>
+
+          <div className=" flex-1 border border-gray-400 rounded-lg p-8 py-7 bg-white mx-2 sm:mx-0 mt-[-80px] sm:mt-0">
+            <p className=" flex items-center gap-2 text-2xl font-semibold text-center ">
+              {docInfo.name}{" "}
+              <img className="w-5" src={assets.verified_icon} alt="" />{" "}
+            </p>
+
+            <div className="flex items-center gap-2 text-sm mt-1 text-gray-600">
+              <p>
+                {docInfo.degree} - {docInfo.speciality}
+              </p>
+              <p className="py-0.5 px-2 border rounded-full bg-[#5f6fff] text-white">
+                {docInfo.experience}{" "}
+              </p>
+            </div>
+            <div>
+              <p className="flex items-center gap-2 font-medium mt-3">
+                About <img src={assets.info_icon} alt="" />{" "}
+              </p>
+              <p className=" text-gray-600 max-w-[700px] mt-2">
+                {docInfo.about}
+              </p>
+            </div>
+            <p className=" text-l text-gray-700 font-medium mt-2">
+              Appointment fee:{" "}
+              <span className=" text-red-600">
+                {currencySymbol} {docInfo.fees}{" "}
+              </span>
+            </p>
+          </div>
+        </div>
+      </div>
+    )
+  );
 };
 
 export default Appointment;
--- frontend/src/pages/Doctor.jsx
+++ frontend/src/pages/Doctor.jsx
@@ -1,10 +1,121 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { AppContext } from "../context/AppContext";
+import DoctorCard from "../components/DoctorCardHome";
 
 const Doctor = () => {
+  const { speciality } = useParams();
+
+  const { doctors } = useContext(AppContext);
+  const [filterDoc, setFilterDoc] = useState([]);
+  const navigate = useNavigate();
+
+  const applyFilter = () => {
+    if (speciality) {
+      setFilterDoc(
+        doctors.filter((doctor) => doctor.speciality === speciality)
+      );
+    } else {
+      setFilterDoc(doctors);
+    }
+  };
+  useEffect(() => {
+    applyFilter();
+  }, [doctors, speciality]);
   return (
     <div>
-      <h1>Doctor Page</h1>
-      <p>Welcome to the Doctor Page</p>
+      <p className=" text-gray-600">Browse through the doctors specialist.</p>
+      <div className=" flex flex-col sm:flex-row items-start gap-5 mt-10 ">
+        <div className=" flex flex-col gap-4 text-gray-700">
+          <p
+            onClick={() =>
+              speciality === "General physician"
+                ? navigate("/doctors")
+                : navigate("/doctors/General physician")
+            }
+            className={`w-[94vw] sm:w-auto pl-3 py-1 pr-16 border border-gray-400 rounded-xl transition-all cursor-pointer 
+              ${
+                speciality === "General physician"
+                  ? "bg-[#5f6fff] text-white"
+                  : ""
+              }`}
+          >
+            General physician
+          </p>
+          <p
+            onClick={() =>
+              speciality === "Gynecologist"
+                ? navigate("/doctors")
+                : navigate("/doctors/Gynecologist")
+            }
+            className={`w-[94vw] sm:w-auto pl-3 py-1 pr-16 border border-gray-400 rounded-xl transition-all cursor-pointer
+               ${
+                 speciality === "Gynecologist" ? "bg-[#5f6fff] text-white" : ""
+               }`}
+          >
+            Gynecologist
+          </p>
+          <p
+            onClick={() =>
+              speciality === "Dermatologist"
+                ? navigate("/doctors")
+                : navigate("/doctors/Dermatologist")
+            }
+            className={`w-[94vw] sm:w-auto pl-3 py-1 pr-16 border border-gray-400 rounded-xl transition-all cursor-pointer
+              ${
+                speciality === "Dermatologist" ? "bg-[#5f6fff] text-white" : ""
+              }`}
+          >
+            Dermatologist
+          </p>
+          <p
+            onClick={() =>
+              speciality === "Pediatricians"
+                ? navigate("/doctors")
+                : navigate("/doctors/Pediatricians")
+            }
+            className={`w-[94vw] sm:w-auto pl-3 py-1 pr-16 border border-gray-400 rounded-xl transition-all cursor-pointer
+               ${
+                 speciality === "Pediatricians" ? "bg-[#5f6fff] text-white" : ""
+               }`}
+          >
+            Pediatricians
+          </p>
+          <p
+            onClick={() =>
+              speciality === "Neurologist"
+                ? navigate("/doctors")
+                : navigate("/doctors/Neurologist")
+            }
+            className={`w-[94vw] sm:w-auto pl-3 py-1 pr-16 border border-gray-400 rounded-xl transition-all 
+               ${
+                 speciality === "Neurologist" ? "bg-[#5f6fff] text-white" : ""
+               } cursor-pointer`}
+          >
+            Neurologist
+          </p>
+          <p
+            onClick={() =>
+              speciality === "Gastroenterologist"
+                ? navigate("/doctors")
+                : navigate("/doctors/Gastroenterologist")
+            }
+            className={`w-[94vw] sm:w-auto pl-3 py-1 pr-16 border 
+               ${
+                 speciality === "Gastroenterologist"
+                   ? "bg-[#5f6fff] text-white"
+                   : ""
+               } border-gray-400 rounded-xl transition-all cursor-pointer`}
+          >
+            Gastroenterologist
+          </p>
+        </div>
+        <div className=" w-full grid grid-cols-[repeat(auto-fill,minmax(200px,1fr))] gap-5 gap-y-6">
+          {filterDoc.map((item, index) => (
+            <DoctorCard key={index} doctor={item} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
